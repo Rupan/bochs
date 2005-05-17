ad2c17cbcbb19b27e6741e4cb70f0b08c66b57d7
Guillem Jover <guillem@debian.org>
2005-05-17 02:20:21 +0000

    Release bochs 2.1.1+20041109-1

---
+++ debian/tarball.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#
+# tarball.sh
+#
+# $Id$
+#
+# Copyright (C) 2004 Guillem Jover <guillem@debian.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+
+set -e
+
+SRCDIR="bochs-snapshot"
+export CVSROOT=":pserver:anonymous@cvs.sf.net:/cvsroot/bochs"
+
+case "$1" in
+  snapshot)
+    echo "-> getting new snapshot."
+    cvs login
+    cvs co -d $SRCDIR bochs
+    ;;
+  update)
+    echo "-> updating snapshot."
+    cvs up -d $SRCDIR bochs
+    ;;
+esac
+
+if ! [ -e $SRCDIR/bochs.h ]
+then
+  echo "error: no bochs source dir available."
+  exit 1
+fi
+
+echo "-> creating new directory tree."
+eval $(grep '^VERSION=' $SRCDIR/configure.in | sed -e 's/\.cvs//' )
+SNAPSHOTVERSION="$VERSION+$(date +%Y%m%d)"
+TARBALLDIR="bochs-$SNAPSHOTVERSION"
+cp -al $SRCDIR $TARBALLDIR
+
+echo "-> cleaning snapshot."
+# Clean non-free stuff
+rm -f $TARBALLDIR/bios/BIOS*
+rm -f $TARBALLDIR/bios/VGABIOS*
+# Clean cvs stuff
+find $TARBALLDIR -name 'CVS' -o -name '.cvsignore' | xargs rm -rf
+
+echo "-> creating new tarball."
+tar czf bochs_$SNAPSHOTVERSION.orig.tar.gz $TARBALLDIR
+
+echo "-> cleaning directory tree."
+rm -rf $TARBALLDIR
+
